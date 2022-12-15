a3fdad30291b3f287436f97f5f26f356a3dc47b2
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-15 23:19:04 +0100

    iteration#1 - Mix for e includes

---
+++ jsReviewBasics.js
@@ -0,0 +1,53 @@
+// **Iteración #1: Mix for e includes**
+
+// Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()**
+
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+let categories = [];
+for (const item of movies) {
+    categories.push(item.categories);
+}
+
+catString = categories.toString();
+
+// console.log(categories);
+// console.log(catString);
+const catArray = catString.split(",");
+// console.log(catArray);
+
+// for (const item of catArray) {
+//     console.log(item)
+// }
+
+const unicos = [];
+
+for(var i = 0; i < catArray.length; i++) {
+ 
+  const elemento = catArray[i];
+ 
+  if (!unicos.includes(catArray[i])) {
+    unicos.push(elemento);
+  }
+}
+
+console.log(unicos); 
+
+
+// const uniqueCategories = [];
+
+// for ( let i = 0; i < categories.length; i++){
+
+//     uniqueCategories.push(categories[i,0]);
+//     for ( let n = 0; n < categories[i].length; n++){
+//         uniqueCategories.push(categories[i,n]);
+//     }
+// }
+
+// console.log(uniqueCategories);
