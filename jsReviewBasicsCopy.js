52675e2b34c84142df18c3c9e2968aea6d61a0c6
omolinsc <oriol.molins@bootcamp-upgrade.com>
2022-12-16 11:05:57 +0100

    iteration#1 optimous result feat Santi

---
+++ jsReviewBasicsCopy.js
@@ -0,0 +1,63 @@
+// **Iteración #1: Mix for e includes**
+
+//? Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función **.includes()**
+
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+let categories = [];
+// bucle que nos saca cada película de la lista de películas
+for (const movie of movies) {
+    // bucle que nos saca cada categoría de nuestra lista de categorias de cada película
+    for ( const categorie of movie.categories){
+        // comprovamos si existe cada categoría, si no está la añadimos, si está no la añadimos
+        !categories.includes(categorie) && categories.push(categorie);
+        }
+}
+// comprobamos el resultado final
+console.log(categories);
+
+// ifesle => condicion ? respuesta correcta : respuesta incorrecta
+// if => condición && respuesta correcta
+
+
+// **Iteración #2: Mix Fors**
+
+//? Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
